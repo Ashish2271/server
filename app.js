874ba1bdc200c82a581fdfb7e7416ebf747322a9
Ashish Pandey <97328833+Ashish2271@users.noreply.github.com>
2024-02-15 13:11:50 +0530

    Update app.js

--- app.js
+++ app.js
@@ -566,7 +566,7 @@ app.post('/email', async (req, res) => {
 app.post('/senderemail', async (req, res) => {
   try {
     const htmlTemplate = `
-asdhgahd<!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.0 Transitional //EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.0 Transitional //EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
     <html xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office">
     <head>
     <!--[if gte mso 9]>
